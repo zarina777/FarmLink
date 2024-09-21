ad507a3da9e7e30c6d9da3a34a89b90de5d58105
zarina777 <uktamovazarinabaratovna@gmail.com>
2024-09-22 03:18:45 +0500

    Some bugs are fixed

--- src/routes/About/components/Section1/index.jsx
+++ src/routes/About/components/Section1/index.jsx
@@ -1,21 +1,34 @@
-import cn from './style.module.scss'
-import ImageWrapper from '../imageWrapper'
-import img from './farmer.webp'
+import cn from "./style.module.scss";
+import ImageWrapper from "../imageWrapper";
+import img from "./farmer.webp";
 const Section1 = () => {
   return (
     <div className={cn.Section1_wrapper}>
-     <div className={cn.box1}>
-      <h4>About Agriculture farm</h4>
-      <h1>Our Company and the Traditions of Agricultural Farming</h1>
-      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repudiandae ullam est quisquam incidunt laudantium deleniti sapiente ab facilis repellendus pariatur molestias, totam dignissimos sunt voluptates, molestiae harum asperiores doloribus atque?</p>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem, sed?</p>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates a perferendis amet nisi aspernatur eos repellat sapiente aperiam ullam debitis?</p>
+      <div className={cn.box1}>
+        <h4>About Agriculture farm</h4>
+        <h1>Our Company and the Traditions of Agricultural Farming</h1>
+        <p>
+          FarmLink is a platform that connects farmers with business owners,
+          enabling reliable transactions and streamlining the supply chain for
+          fresh, local products, fostering sustainable growth in the
+          agriculture-business ecosystem
+        </p>
+        <p>
+          It reduces the time consumption of searching for the best farmers with high quality product.
+        </p>
+        <p>
+          We provide an opportunity to business owners connecting directly with
+          farmers offering fresh, high-quality products while ensuring
+          efficiency and value in every transaction
+        </p>
+      </div>
+      <ImageWrapper
+        src={img}
+        alt="Image about farm"
+        className={cn.ImageWrapper}
+      />
     </div>
-      <ImageWrapper src={img} alt="Image about farm" className={cn.ImageWrapper} />
+  );
+};
 
-    </div>
-   
-  )
-}
-
-export default Section1
+export default Section1;
--- src/routes/About/components/Section2/index.jsx
+++ src/routes/About/components/Section2/index.jsx
@@ -1,51 +1,62 @@
-import cn from './style.module.scss'
-import Card from '../card'
-import ImageWrapper from '../imageWrapper'
-import rasm1 from '../images/category-1.png'
-import rasm2 from '../images/category-2.png'
-import rasm3 from '../images/category-3.png'
-import rasm4 from '../images/category-4.png'
-
+import cn from "./style.module.scss";
+import Card from "../card";
+import ImageWrapper from "../imageWrapper";
+import rasm1 from "../images/category-1.png";
+import rasm2 from "../images/category-2.png";
+import rasm3 from "../images/category-3.png";
+import rasm4 from "../images/category-4.png";
 
 const Section2 = () => {
   return (
-   <>
-   <div className={cn.section2_container}>
-   <div className={cn.text}>
-    <div className={cn.part1}>
-        <h4>What We’re Doing</h4>
-        <h2>Services We Provide</h2>
-    </div>
-    <div className={cn.part2}>
-    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ad doloremque quam, quia maxime nisi, laboriosam perspiciatis voluptate corporis molestias totam veniam libero ex fugit commodi facilis blanditiis cupiditate quasi quae.
-    </div>
-   </div>
-   <div className={cn.cards}>
-   <Card 
-                title="Card Title"
-                content="This is a brief description of the card content."
-                className={cn.card_box}
-            ><ImageWrapper src={rasm1} alt='this is photo' className={cn.card}/></Card>
-   <Card 
-                title="Card Title"
-                content="This is a brief description of the card content."
-                className={cn.card_box}
-            ><ImageWrapper src={rasm2} alt='this is photo'className={cn.card}/></Card>
-   <Card 
-                title="Card Title"
-                className={cn.card_box}
-                content="This is a brief description of the card content."
-            ><ImageWrapper src={rasm3} alt='this is photo' className={cn.card}/></Card>
-   <Card 
-                title="Card Title"
-                className={cn.card_box}
-                content="This is a brief description of the card content."
-            ><ImageWrapper src={rasm4} alt='this is photo' className={cn.card}/></Card>
-   </div>
-  
-   </div>
-   </>
-  )
-}
+    <>
+      <div className={cn.section2_container}>
+        <div className={cn.text}>
+          <div className={cn.part1}>
+            <h4>What We’re Doing</h4>
+            <h2>Services We Provide</h2>
+          </div>
+          <div className={cn.part2}>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ad
+            doloremque quam, quia maxime nisi, laboriosam perspiciatis voluptate
+            corporis molestias totam veniam libero ex fugit commodi facilis
+            blanditiis cupiditate quasi quae.
+          </div>
+        </div>
+        <div className={cn.cards}>
+          <Card
+            title="Find "
+            content="Most reliable farmers and product holders
+                "
+            className={cn.card_box}
+          >
+            <ImageWrapper src={rasm1} alt="this is photo" className={cn.card} />
+          </Card>
+          <Card
+            title="Simplify"
+            content="Logistics for faster delivery, no store fee"
+            className={cn.card_box}
+          >
+            <ImageWrapper src={rasm2} alt="this is photo" className={cn.card} />
+          </Card>
+          <Card
+            title="Buy"
+            className={cn.card_box}
+            content="Fresh and first-hand products at affordable price"
+          >
+            <ImageWrapper src={rasm3} alt="this is photo" className={cn.card} />
+          </Card>
+          <Card
+            title="Success"
+            className={cn.card_box}
+            content="Maintain your business, finding potential customers"
+          >
+            <ImageWrapper src={rasm4} alt="this is photo" className={cn.card} />
+          </Card>
+          
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default Section2
+export default Section2;
